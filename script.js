a127efab09fae49a660ae7834ba4bd803dad9444
Sebastian <sebassha@stud.ntnu.no>
2023-04-20 13:09:15 +0200

    Win condition works, but lacks fail condition

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ correctBoxes.forEach(function(correctBox) {
   });
 });
 
+const boxes = document.querySelectorAll('.box');
+const boxCorrect = document.querySelectorAll('.box.correct');
+const winScreen = document.querySelector('.win-screen');
+
+correctBoxes.forEach((box) => {
+  box.addEventListener('mouseover', function() {
+    box.classList.add('locked');
+
+    const lockedBoxes = document.querySelectorAll('.box.correct.locked');
+    if (lockedBoxes.length === correctBoxes.length) {
+      showWinScreen();
+    }
+  });
+});
+
+function showWinScreen() {
+  winScreen.classList.add('show');
+}
+
