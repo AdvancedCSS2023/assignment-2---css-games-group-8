b876489f78fbe734606ce949f0276ed769bf912e
Sebastian <sebassha@stud.ntnu.no>
2023-04-21 14:41:51 +0200

    Tried adding a fail state if incorrect guess

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 const correctBoxes = document.querySelectorAll(".box--correct");
+const boxes = document.querySelectorAll(".box");
+const winScreen = document.querySelector(".win-screen");
+const loseScreen = document.querySelector(".lose-screen");
+let incorrectGuesses = 0;
 
 correctBoxes.forEach(function (correctBox) {
   correctBox.addEventListener("mouseover", function () {
     correctBox.classList.add("box--locked");
-  });
-});
-
-const boxes = document.querySelectorAll(".box");
-const boxCorrect = document.querySelectorAll(".box--correct");
-const winScreen = document.querySelector(".win-screen");
-
-correctBoxes.forEach((box) => {
-  box.addEventListener("mouseover", function () {
-    box.classList.add("box--locked");
 
     const lockedBoxes = document.querySelectorAll(".box--correct.box--locked");
     if (lockedBoxes.length === correctBoxes.length) {
@@ -21,6 +15,24 @@ correctBoxes.forEach((box) => {
   });
 });
 
+boxes.forEach(function (box) {
+  if (!box.classList.contains("box--correct")) {
+    box.addEventListener("mouseover", function () {
+      incorrectGuesses++;
+
+      if (incorrectGuesses === 3) {
+        showLoseScreen();
+      }
+    });
+  }
+});
+
 function showWinScreen() {
   winScreen.classList.add("win-screen--show");
 }
+
+function showLoseScreen() {
+  loseScreen.classList.add("lose-screen--show");
+}
+
+
